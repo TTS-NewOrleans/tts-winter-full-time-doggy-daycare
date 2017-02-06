1f5f405026f984bfb3284649d56cbe4979c731f9
Ryan Hageman <improv42@gmail.com>
2017-02-06 10:21:53 -0600

    Add Update to Dogs Controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -26,6 +26,17 @@ class DogsController < ApplicationController
     @dog = Dog.find(params[:id])
   end
 
+  def update
+    @dog = Dog.find(params[:id])
+
+    if @dog.update(dog_params)
+      redirect_to dogs_url, notice: "Successfully updated #{@dog.name}."
+    else
+      flash.now[:notice] = "Something went wrong. We couldn't update #{@dog.name}"
+      render :edit
+    end
+  end
+
   private
 
   def dog_params
